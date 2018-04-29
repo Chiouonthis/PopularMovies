14f120b0719fbceadc9adfcb5753f6bc567cf191
chiouonthis <jchiou1984@gmail.com>
2018-04-29 11:44:36 -0400

    Working on RecyclerView Adapter and View Holder

---
+++ app/src/main/java/com/chiouonthis/popularmovies/MoviePosterAdapter.java
@@ -0,0 +1,35 @@
+package com.chiouonthis.popularmovies;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MoviePosterAdapter extends RecyclerView.Adapter<MoviePosterAdapter.MovieViewHolder> {
+
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+
+    }
+
+}
+
+
+
+
+
