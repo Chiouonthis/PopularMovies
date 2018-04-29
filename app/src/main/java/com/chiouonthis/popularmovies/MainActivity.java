79ea34d43c449098940303023ff29502ab0a14e2
chiouonthis <jchiou1984@gmail.com>
2018-04-29 13:16:11 -0400

    Working on implementing the grid layout manager in Acitivity Main.

--- app/src/main/java/com/chiouonthis/popularmovies/MainActivity.java
+++ app/src/main/java/com/chiouonthis/popularmovies/MainActivity.java
@@ -2,12 +2,32 @@ package com.chiouonthis.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    MoviePosterAdapter moviePosterAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: Create dummy data to test population of image views
+
+        RecyclerView recyclerView = findViewById(R.id.rvMoviePosters);
+        int numberOfColumns = 3; //TODO: Make this dynamic depending on screen orientation
+        recyclerView.setLayoutManager(new GridLayoutManager(this,numberOfColumns));
+        moviePosterAdapter = new MoviePosterAdapter(numberOfColumns);
+
+        recyclerView.setAdapter(moviePosterAdapter);
+
+
     }
+
+
 }
