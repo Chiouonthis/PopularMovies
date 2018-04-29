13da60e5a3cf1f36631c9507c77de166bf6110af
chiouonthis <jchiou1984@gmail.com>
2018-04-29 12:21:30 -0400

    Continued work on the adapter for RecyclerView.

--- app/src/main/java/com/chiouonthis/popularmovies/MoviePosterAdapter.java
+++ app/src/main/java/com/chiouonthis/popularmovies/MoviePosterAdapter.java
@@ -1,10 +1,19 @@
 package com.chiouonthis.popularmovies;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MoviePosterAdapter extends RecyclerView.Adapter<MoviePosterAdapter.MovieViewHolder> {
+
+    private int itemCount;
+
+    public MoviePosterAdapter(int itemCount) {
+        this.itemCount = itemCount;
+    }
 
 
     @NonNull
@@ -25,7 +34,25 @@ public class MoviePosterAdapter extends RecyclerView.Adapter<MoviePosterAdapter.
 
     class MovieViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView moviePosterImageView;
+
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+
+            moviePosterImageView = (ImageView) itemView.findViewById(R.id.ivMoviePosterImage);
+        }
+
+
+        void bind(int listIndex) {
+
+            //TODO complete method body to set the image of the image view from API response
+
+        }
+
     }
+
+
 
 }
 
