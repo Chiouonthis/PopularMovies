f256eb6134e9490188895eca4583c8b0b1d087bd
chiouonthis <jchiou1984@gmail.com>
2018-05-03 23:15:33 -0400

    I think I fixed the popular movies URL and have the request working.

--- app/src/main/java/com/chiouonthis/popularmovies/MainActivity.java
+++ app/src/main/java/com/chiouonthis/popularmovies/MainActivity.java
@@ -11,14 +11,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements MoviePosterAdapter.PosterClickListener, NetworkUtils.MovieDBService{
 
@@ -28,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements MoviePosterAdapte
     private ImageView moviePosterImage;
     private static final String TAG = "MainActivity ";
     private static int numberOfColumns = 3; //TODO: Make this dynamic depending on screen orientation
-    private static final String BASE_API_URL = "https://api.themoviedb.org";
     private TextView moviePosterTitle;
 
     @Override
@@ -45,35 +41,8 @@ public class MainActivity extends AppCompatActivity implements MoviePosterAdapte
 
         posterRecyclerView.setAdapter(moviePosterAdapter);
 
-        try {
-
-            Response<List<Movie>> listResponse = listMovies(getResources().getString(R.string.MovieDbAPIKey)).enqueue(new Callback<List<Movie>>() {
-                @Override
-                public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
-                    response = response.body();
-                }
-
-                @Override
-                public void onFailure(Call<List<Movie>> call, Throwable t) {
-
-                }
-            });
-            Log.d(TAG, listResponse.toString());
-
-            // TODO Parse JSON?
-            List<Movie> movies = listResponse.body();
-            for (int i = 0; i < movies.size(); i++) {
-
-                moviePosterImage = findViewById(R.id.ivMoviePosterImage);
-                moviePosterTitle = findViewById(R.id.tvMovieTitle);
-                // EXAMPLE http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg
-                moviePosterTitle.setText(movies.get(i).getTitle());
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        listMovies(getResources().getString(R.string.MovieDbAPIKey));
+        
 
     }
 
@@ -108,17 +77,27 @@ public class MainActivity extends AppCompatActivity implements MoviePosterAdapte
     @Override
     public Call<List<Movie>> listMovies(String apiKey) {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
         NetworkUtils.MovieDBService service = retrofit.create(NetworkUtils.MovieDBService.class);
         Call<List<Movie>> popularMoviesList = service.listMovies(apiKey);
 
+        popularMoviesList.enqueue(new Callback<List<Movie>>() {
+            @Override
+            public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
+
+                Log.d(TAG, response.toString());
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Movie>> call, Throwable t) {
+
+            }
+        });
+
         return popularMoviesList;
     }
-    
+
 
 }
 
